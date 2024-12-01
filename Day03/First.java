857dc3cdbb32f32e3bfcc3c37ca548d8641700a3
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2024-12-01 19:59:33 +0100

    2024

--- Day03/First.java
+++
@@ -1,7 +0,0 @@
-package Day03;
-import java.
-public class First {
-    public static void main(String[] args) {
-        FileReader fr = new FileReader("/home/seba/workspace/Advent-of-code-2023/Day03/input.txt");
-    }
-}
